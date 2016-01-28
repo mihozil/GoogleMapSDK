1935063d65c1d81dacc184463bac694f0f1549b6
Apple <Apple@Apples-MacBook-Air.local>
2016-01-28 16:19:47 +0700

    Update

---
+++ MAPIOS/DetailScreen.h
@@ -0,0 +1,13 @@
+//
+//  DetailScreen.h
+//  MAPIOS
+//
+//  Created by Apple on 1/28/16.
+//  Copyright © 2016 AMOSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailScreen : UIViewController
+
+@end
